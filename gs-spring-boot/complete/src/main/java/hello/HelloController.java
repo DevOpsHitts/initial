d6a8645cc505af15cfc0fd0e0318f01582f127c4
DevOpsHitts <victor.torres.isc@gmail.com>
2016-01-21 15:48:17 -0600

    another modification

--- gs-spring-boot/complete/src/main/java/hello/HelloController.java
+++ gs-spring-boot/complete/src/main/java/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot! modified";
+        return "Greetings from Spring Boot! modified again";
     }
     
 }
